5d3e06f3add607f28f87da8e4e0cbd15ca8f47c2
Marcel Mukundi <45983298+MMukundi@users.noreply.github.com>
2021-09-28 16:31:40 -0700

    Added data-slot loading

--- src/main/java/com/jam/jamcougstatistics/DataLoader.java
+++ src/main/java/com/jam/jamcougstatistics/DataLoader.java
@@ -4,6 +4,61 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class DataLoader {
+    public static class DataSlots<T extends Number> {
+        ArrayList<ArrayList<T>> slots;
+
+        DataSlots(int size) {
+            slots = new ArrayList<>(size);
+        }
+
+        public void padToSize(int size) {
+            for (int i = slots.size(); i < size; i++)
+                slots.add(null);
+        }
+
+        public void clear() {
+            slots.clear();
+        }
+
+        public void setSlot(int slotIndex, ArrayList<T> values) {
+            padToSize(slotIndex + 1);
+            slots.set(slotIndex, values);
+        }
+
+        public boolean hasSlot(int slotIndex) {
+            return (slotIndex < slots.size()) && (slots.get(slotIndex) != null);
+        }
+
+        public void addToSlot(int slotIndex, T t) {
+            // Make sure the list is long enough
+            padToSize(slotIndex + 1);
+
+            // Make sure the slot is initialized
+            ArrayList<T> values = slots.get(slotIndex);
+            if (values == null) {
+                values = new ArrayList<>();
+                slots.set(slotIndex, values);
+            }
+            // Add to the slot
+            values.add(t);
+        }
+
+
+        public void assignSlotsFrom(DataSlots<T> otherSlots) {
+            int otherSize = otherSlots.slots.size();
+            int thisSize = slots.size();
+            int minSize = Math.min(otherSize, thisSize);
+            for (int overlapSlotIndex = 0; overlapSlotIndex < minSize; overlapSlotIndex++) {
+                if (otherSlots.hasSlot(overlapSlotIndex)) {
+                    slots.set(overlapSlotIndex, otherSlots.slots.get(overlapSlotIndex));
+                }
+            }
+            for (int extraSlotIndex = otherSize; extraSlotIndex < thisSize; extraSlotIndex++) {
+                slots.add(otherSlots.slots.get(extraSlotIndex));
+            }
+        }
+    }
+
     public static boolean IsValid(File file) throws FileNotFoundException {
         InputStream ifstream = new FileInputStream(file);
         InputStreamReader ireader = new InputStreamReader(ifstream);
@@ -52,19 +107,34 @@ public class DataLoader {
             } catch (Exception e) {
                 throw new Exception();
             }
-
-
         }
         return ret;
     }
 
-    public static ArrayList<Integer>[] LoadDoubleDataSet(File file) throws Exception {
+    public static DataSlots<Integer> LoadDataSet(File file) throws Exception {
         InputStream ifstream = new FileInputStream(file);
         InputStreamReader ireader = new InputStreamReader(ifstream);
         BufferedReader reader = new BufferedReader(ireader);
-        ArrayList<Integer>[] ret = new ArrayList[2];
-        ret[0] = new ArrayList<>();
-        ret[1] = new ArrayList<>();
+
+        DataSlots<Integer> data = new DataSlots<>(2);
+        for (String line : reader.lines().toArray(String[]::new)) {
+            String[] splitLine = line.split(",");
+            for (int i = 0; i < splitLine.length; i++) {
+                if (splitLine[i].length() == 0) {
+                    continue;
+                }
+                data.addToSlot(i, Integer.parseInt(splitLine[i]));
+            }
+        }
+        return data;
+    }
+
+    public static DataSlots<Integer> LoadDoubleDataSet(File file) throws Exception {
+        InputStream ifstream = new FileInputStream(file);
+        InputStreamReader ireader = new InputStreamReader(ifstream);
+        BufferedReader reader = new BufferedReader(ireader);
+
+        DataSlots<Integer> data = new DataSlots<>(2);
         for (String line : reader.lines().toArray(String[]::new)) {
             String[] splitLine = line.split(",");
             if (splitLine.length != 2) throw new Exception();
@@ -72,9 +142,9 @@ public class DataLoader {
                 if (splitLine[i].length() == 0) {
                     continue;
                 }
-                ret[i].add(Integer.parseInt(splitLine[i]));
+                data.addToSlot(i, Integer.parseInt(splitLine[i]));
             }
         }
-        return ret;
+        return data;
     }
 }
--- src/main/java/com/jam/jamcougstatistics/HelloController.java
+++ src/main/java/com/jam/jamcougstatistics/HelloController.java
@@ -4,12 +4,18 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.List;
 
 public class HelloController {
+    @FXML
+    ToggleGroup dataSlot;
+
+    DataLoader.DataSlots<Integer> slots = new DataLoader.DataSlots<>(2);
     @FXML
     private Label welcomeText;
 
@@ -22,18 +28,30 @@ public class HelloController {
     protected void onTestClick(){}
 
     @FXML
-    protected void LoadDataClick(){
+    protected void LoadDataClick() throws Exception {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open data set");
         File selected_file = fileChooser.showOpenDialog(new Stage());
         if (selected_file == null){
             Alert warning = new Alert(Alert.AlertType.ERROR, "Error file unable to be opened.", ButtonType.CLOSE);
             warning.showAndWait();
-        }
-
-        if (!selected_file.getName().endsWith(".csv")) {
+        }else if (!selected_file.getName().endsWith(".csv")) {
             Alert warning = new Alert(Alert.AlertType.ERROR, "File must be a csv file.", ButtonType.CLOSE);
             warning.showAndWait();
         }
+
+        DataLoader.DataSlots<Integer> loadedSlots = DataLoader.LoadDataSet(selected_file);
+        // If only one dataset is loaded in...
+        if(!loadedSlots.hasSlot(1)){
+            // Load it into the selected slot
+            int index = dataSlot.getToggles().indexOf(dataSlot.getSelectedToggle());
+            slots.setSlot(index, loadedSlots.slots.get(0));
+        }else{
+            // Otherwise, load both in
+            slots.assignSlotsFrom(loadedSlots);
+        }
+        String s1 = slots.hasSlot(0)?slots.slots.get(0).toString():"null";
+        String s2 = slots.hasSlot(1)?slots.slots.get(1).toString():"null";
+        System.out.printf("Slot 1:%s\nSlot 2:%s\n",s1,s2);
     }
 }
